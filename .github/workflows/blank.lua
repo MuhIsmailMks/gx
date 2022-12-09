345eacc27f8b2794cd8b3312ac4e43cf9fab7cc4
MuhIsmailMks <116533503+MuhIsmailMks@users.noreply.github.com>
2022-12-09 18:20:49 +0800

    Update blank.lua

--- .github/workflows/blank.lua
+++ .github/workflows/blank.lua
@@ -851,11 +851,10 @@ function huun()
         hii = ":red_circle:"
     end
     local ggh = [[
-    $RAM = [math]::Round($Memory, 0)
     $CPU = Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average | Select -ExpandProperty Average
         $webHookUrl = "]]..cekdcs..[[/messages/]]..iddcs..[["
         $payload = @{
-            content = "]]..rdpnam..[[ - ]]..Bot[getBot().name:upper()].slot..[[ ]]..hii..[[ ]]..hiik..[[ ]]..(os.date("!%H:%M", os.time() + 7 * 60 * 60))..[[ | ]]..gm2..[[   ]]..gm3..[[   ]]..gm4..[[  | $CPU%   $RAM%"
+            content = "]]..rdpnam..[[ - ]]..Bot[getBot().name:upper()].slot..[[ ]]..hii..[[ ]]..hiik..[[ ]]..(os.date("!%H:%M", os.time() + 7 * 60 * 60))..[[ | ]]..gm2..[[   ]]..gm3..[[   ]]..gm4..[[  | $CPU%"
         }
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
         Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Patch -ContentType 'application/json'
